ce341fde48bd9038a52e9a5d8e70313d6c657e9e
anikaweinmann <aweinmann@mundialis.de>
2022-03-17 11:56:13 +0100

    add template

--- src/actinia_tiling_plugin/api/tiling/tiling_grid.py
+++ src/actinia_tiling_plugin/api/tiling/tiling_grid.py
@@ -40,7 +40,9 @@ from actinia_core.rest.resource_base import ResourceBase
 from actinia_core.core.common.redis_interface import enqueue_job
 from actinia_core.core.common.app import URL_PREFIX
 
-from actinia_core.rest.mapset_management import MapsetManagementResourceUser
+from actinia_core.rest.vector_layer import VectorLayerResource
+from actinia_core.core.common.process_chain import ProcessChainConverter
+
 
 from actinia_tiling_plugin.apidocs import helloworld
 from actinia_tiling_plugin.resources.templating import tplEnv
@@ -59,6 +61,7 @@ class AsyncTilingProcessGridResource(ResourceBase):
             mapset_name=mapset_name,
         )
         if rdc:
+            # for debugging use the following to lines instead of enqueue_job
             processing = AsyncTilingProcessGrid(rdc)
             processing.run()
             # enqueue_job(self.job_timeout, start_job, rdc)
@@ -91,9 +94,42 @@ class AsyncTilingProcessGrid(PersistentProcessing):
         # TODO RESPONSEMODEL
         # self.response_model_class = STRDSSampleGeoJSONResponseModel
 
-    def _execute(self):
+    def _execute_preparation(self):
+
         self._setup()
 
+        # Check and lock the target and temp mapsets
+        self._check_lock_target_mapset()
+
+        if self.target_mapset_exists is False:
+            # Create the temp database and link the
+            # required mapsets into it
+            self._create_temp_database(self.required_mapsets)
+
+            # Initialize the GRASS environment and switch into PERMANENT
+            # mapset, which is always linked
+            self._create_grass_environment(
+                grass_data_base=self.temp_grass_data_base,
+                mapset_name="PERMANENT"
+            )
+
+            # Create the temporary mapset with the same name as the target
+            # mapset and switch into it
+            self._create_temporary_mapset(
+                temp_mapset_name=self.target_mapset_name,
+                interim_result_mapset=None,
+                interim_result_file_path=None)
+            self.temp_mapset_name = self.target_mapset_name
+        else:
+            # Init GRASS environment and create the temporary mapset
+            self._create_temporary_grass_environment(
+                source_mapset_name=self.target_mapset_name)
+            self._lock_temp_mapset()
+
+    def _execute(self):
+
+        self._execute_preparation()
+        pconv = ProcessChainConverter()
         # mapset_mr = MapsetManagementResourceUser()
         # mapset_resp = mapset_mr.get(self.location_name, self.mapset_name)
         # mapset_info = json.loads(mapset_resp.data)["process_results"]
@@ -104,11 +140,14 @@ class AsyncTilingProcessGrid(PersistentProcessing):
         grid_name = f"grid_{uuid4().hex}"
         box = f"{req_data_orig['width']},{req_data_orig['height']}"
         tpl1 = tplEnv.get_template("pc_create_grid.json")
-        self.request_data = json.loads(tpl1.render(
+        pc1 = json.loads(tpl1.render(
             box=box,
             grid_name=grid_name
         ).replace('\n', '').replace(" ", ""))
-        PersistentProcessing._execute(self, skip_permission_check=True)
+        pl1 = pconv.process_chain_to_process_list(pc1)
+        self.output_parser_list = pconv.output_parser_list
+        self._execute_process_list(pl1)
+        self._parse_module_outputs()
         grid_info = self.module_results["grid_info"]
         num_grid_cells = int([
             info.split("=")[1] for info in grid_info
@@ -117,18 +156,26 @@ class AsyncTilingProcessGrid(PersistentProcessing):
 
         # extract grid cells
         tpl2 = tplEnv.get_template("pc_extract_grid.json")
-        self.request_data = json.loads(tpl2.render(
+        pc2 = json.loads(tpl2.render(
             grid_name=grid_name,
             grid_prefix=grid_prefix,
             n=num_grid_cells
         ).replace('\n', '').replace(" ", ""))
-        PersistentProcessing._execute(self, skip_permission_check=True)
+        pl2 = pconv.process_chain_to_process_list(pc2)
+        self._execute_process_list(pl2)
 
         # delete grid
+        vect_mng = VectorLayerResource()
+        vect_mng.delete(self.location_name, self.mapset_name, grid_name)
+        tpl3 = tplEnv.get_template("pc_delete_vector.json")
+        pc3 = json.loads(tpl3.render(
+            vector_name=grid_name
+        ).replace('\n', '').replace(" ", ""))
+        pl3 = pconv.process_chain_to_process_list(pc3)
+        self._execute_process_list(pl3)
 
         # make response pretty
-
-
+        import pdb; pdb.set_trace()
 
         # point_file = tempfile.NamedTemporaryFile(dir=self.temp_file_path, delete=True)
         # result_file = tempfile.NamedTemporaryFile(dir=self.temp_file_path, delete=True)
