7c408f8a8ccf1414a67eb5994caef094c8180adb
Carmen <carmen.tawalika@gmail.com>
2024-06-20 14:01:24 +0200

    fix name

--- src/actinia_tiling_plugin/__init__.py
+++ src/actinia_tiling_plugin/__init__.py
@@ -1,35 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Copyright (c) 2018-2022 mundialis GmbH & Co. KG
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-actinia plugin initalization
-"""
-
-__license__ = "GPLv3"
-__author__ = "Carmen Tawalika, Anika Weinmann"
-__copyright__ = "Copyright 2018-2022 mundialis GmbH & Co. KG"
-__maintainer__ = "mundialis GmbH % Co. KG"
-
-
 import importlib.metadata
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = 'actinia_metadata_plugin.wsgi'
+    dist_name = ___name__
     __version__ = importlib.metadata.version(dist_name)
 except Exception:
     __version__ = 'unknown'
