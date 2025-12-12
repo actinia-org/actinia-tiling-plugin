f13801613ef25a865da3b4446816ca7fbc82b41a
Markus Neteler <neteler@gmail.com>
2025-12-12 15:38:02 +0100

    tests_with_kvdb.sh: minor cross-repo var declaration sync

--- tests_with_kvdb.sh
+++ tests_with_kvdb.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env sh
+########################################################################
+#
+# MODULE:       tests_with_kvdb.sh
+#
+# AUTHOR(S):    Anika Weinmann
+#               mundialis GmbH & Co. KG, Bonn
+#               https://www.mundialis.de
+#
+# PURPOSE:      This script sets up a test environment including the kvdb (valkey) server
+#
+# SPDX-FileCopyrightText: (c) 2022 by mundialis GmbH & Co. KG
+#
+# REQUIREMENTS: sudo apt install valkey-server
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+########################################################################
 
-# start valkey server
+# start kvdb server
 valkey-server &
 sleep 1
 valkey-cli ping
@@ -10,23 +27,26 @@ valkey-cli ping
 # sleep 10
 
 # run tests
-echo "$ACTINIA_CUSTOM_TEST_CFG"
-echo "$DEFAULT_CONFIG_PATH"
+echo "${ACTINIA_CUSTOM_TEST_CFG}"
+echo "${DEFAULT_CONFIG_PATH}"
 
-if [ "$1" = "dev" ]
+if [ "$1" == "dev" ]
 then
   echo "Executing only 'dev' tests ..."
-  pytest -m dev
-elif [ "$1" = "integrationtest" ]
+  pytest -m "dev"
+elif [ "$1" == "integrationtest" ]
 then
-  pytest -m integrationtest
+  pytest -m "integrationtest"
 else
   pytest
 fi
 
 TEST_RES=$?
 
-# stop valkey server
+# stop kvdb server
 valkey-cli shutdown
+
+# # stop webhook server
+# curl http://0.0.0.0:5005/shutdown
 
 return $TEST_RES
